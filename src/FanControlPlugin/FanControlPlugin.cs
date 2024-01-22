09c86e2943f5d4b02850ea637840b6b8e1a23df9
gmoorec <157329488+gmoorec@users.noreply.github.com>
2024-01-21 20:54:33 -0500

    Update FanControlPlugin.cs

--- src/FanControlPlugin/FanControlPlugin.cs
+++ src/FanControlPlugin/FanControlPlugin.cs
@@ -1,21 +1,15 @@
-﻿using FanControl.Plugins;
+using FanControl.Plugins;
 using System;
+using System.Collections.Generic;
 
 namespace FanControl.SerialComSensor
 {
     public class FanControlPlugin : IPlugin
     {
-        public string Name => "FanControlPlugin.SerialCOMSensor";
-
         private COMReader reader;
+        private TempSensor temp1Sensor;
+        private TempSensor temp2Sensor;
 
-        public void Close()
-        {
-            reader.Dispose();
-            reader = null;
-        }
-
-        [System.Obsolete]
         public void Initialize()
         {
             string com = Environment.GetEnvironmentVariable("SENSOR_COM_PORT");
@@ -23,12 +17,23 @@ namespace FanControl.SerialComSensor
             {
                 throw new Exception("SENSOR_COM_PORT variable is not set!");
             }
+
             reader = new COMReader(com);
+            temp1Sensor = new TempSensor(reader, "temp1");
+            temp2Sensor = new TempSensor(reader, "temp2");
+        }
+
+        public void Close()
+        {
+            reader.Dispose();
         }
 
         public void Load(IPluginSensorsContainer _container)
         {
-            _container.TempSensors.Add(new SerialCOMSensor(reader));
+            _container.TempSensors.Add(temp1Sensor);
+            _container.TempSensors.Add(temp2Sensor);
         }
+
+        public string Name => "FanControlPlugin.SerialCOMSensor";
     }
 }
