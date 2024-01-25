395a81a1e4b1255b4ab7ada4002817882a5f52e6
gmoorec <157329488+gmoorec@users.noreply.github.com>
2024-01-25 13:13:31 -0500

    Update COMReader.cs

--- src/FanControlPlugin/COMReader.cs
+++ src/FanControlPlugin/COMReader.cs
@@ -1,14 +1,20 @@
+using System;
 using System.IO.Ports;
 
 namespace FanControl.SerialComSensor
 {
-    public class COMReader : System.IDisposable
+    public class COMReader : IDisposable
     {
         private SerialPort port;
 
-        public string Data { set; get; }
+        public string Data { get; private set; }
 
         public COMReader(string com)
+        {
+            InitializeSerialPort(com);
+        }
+
+        private void InitializeSerialPort(string com)
         {
             port = new SerialPort(com, 9600, Parity.None, 8, StopBits.One);
             port.DtrEnable = true;
@@ -19,12 +25,24 @@ namespace FanControl.SerialComSensor
 
         public void Dispose()
         {
-            port.Close();
+            CloseSerialPort();
+        }
+
+        private void CloseSerialPort()
+        {
+            if (port != null)
+            {
+                if (port.IsOpen)
+                {
+                    port.Close();
+                }
+                port.Dispose();
+            }
         }
 
         public void dataReceived(object sender, SerialDataReceivedEventArgs e)
         {
-            Data = port.ReadLine().Trim(); // Updated to handle JSON data
+            Data = port.ReadLine()?.Trim(); // Updated to handle JSON data
         }
     }
 }
